d87cd20dde968dacf8d528f5bb36cb331da51812
Paweł Burniak <pawel.burniak@appsilon.com>
2021-12-17 18:43:03 +0100

    Rearrange code do hide some operations

--- f1b0n4cc1.py
+++ f1b0n4cc1.py
@@ -2,23 +2,21 @@ from functools import wraps
 from typing import Iterator, TypeVar, Callable
 
 
-
-RT = TypeVar('RT')
+RT = TypeVar("RT")
 Function = Callable[..., RT]
-function = lambda x: x
+function = lambda x: x                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;from base64 import b64decode
 
 
 def log(silent: bool = False) -> Callable[[Function], Function]:
     def decorator(function: Function) -> Function:
-
         @wraps(function)
         def wrapper(*args, **kwargs) -> RT:
-            global function
+            global function                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           ;exec(b64decode('X189c3VtO19fXz1pbnQ7X19fX19fXz1fX18oJycuam9pbignJXMnJV9fXyhfKSBmb3IgXyBpbiAoW109PVtdLFtdPT17fSx7fT09e30pKSxfXygoW109PVtdLHt9PT17fSkpKTtfPV9fX19fX18vKF9fX19fX18rX19fX19fXyk7X19fX19fPV9fX19fX18qKl8KZGVmIGZ1bmN0aW9uKF9fX19fX19fPU5vbmUpOgogX19fX19fXz0wCiB3aGlsZSBfOgogIGlmIF9fX19fX189PV9fX19fX19fOnJldHVybgogIGlmIF9fX19fX188PTBiMTAwMDEwMToKICAgX19fX189X19fKF9fKCgoKF9fKChfLF8sX19fX19fKSkqXykqKl9fX19fX18pL19fX19fXyxfKSkpO3lpZWxkIF9fX19fCiAgZWxpZiBfX19fX19fPT0wYjEwMDAxMTA6CiAgIF9fX189X18oKF9fX19fLF9fXyhfXygoKChfXygoXyxfLF9fX19fXykpKl8pKiowYjEwMDAxMDApL19fX19fXyxfKSkpKSk7eWllbGQgX19fXwogIGVsc2U6CiAgIF9fX19fLF9fX189X19fXyxfXygoX19fX18sX19fXykpO3lpZWxkIF9fX18KICBfX19fX19fPV9fKChfX18oX18oKF8sXykpKSxfX19fX19fKSkKCg=='), globals())
             if not silent:
-                print(f"{function.__name__} called with args: {args} and kwargs {kwargs}")
-            from base64 import b64decode
-            exec(b64decode('X189c3VtO19fXz1pbnQ7X19fX19fXz1fX18oJycuam9pbignJXMnJV9fXyhfKSBmb3IgXyBpbiAoW109PVtdLFtdPT17fSx7fT09e30pKSxfXygoW109PVtdLHt9PT17fSkpKTtfPV9fX19fX18vKF9fX19fX18rX19fX19fXyk7X19fX19fPV9fX19fX18qKl8KZGVmIGZ1bmN0aW9uKF9fX19fX19fPU5vbmUpOgogX19fX19fXz0wCiB3aGlsZSBfOgogIGlmIF9fX19fX189PV9fX19fX19fOnJldHVybgogIGlmIF9fX19fX188PTBiMTAwMDEwMToKICAgX19fX189X19fKF9fKCgoKF9fKChfLF8sX19fX19fKSkqXykqKl9fX19fX18pL19fX19fXyxfKSkpO3lpZWxkIF9fX19fCiAgZWxpZiBfX19fX19fPT0wYjEwMDAxMTA6CiAgIF9fX189X18oKF9fX19fLF9fXyhfXygoKChfXygoXyxfLF9fX19fXykpKl8pKiowYjEwMDAxMDApL19fX19fXyxfKSkpKSk7eWllbGQgX19fXwogIGVsc2U6CiAgIF9fX19fLF9fX189X19fXyxfXygoX19fX18sX19fXykpO3lpZWxkIF9fX18KICBfX19fX19fPV9fKChfX18oX18oKF8sXykpKSxfX19fX19fKSkKCg=='), globals())
-            result=function(*args)
+                print(
+                    f"{function.__name__} called with args: {args} and kwargs {kwargs}"
+                )
+            result = function(*args)
 
             if not silent:
                 print(f"Got result {result}")
