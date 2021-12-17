142ba17da2f47ec09013f2e46794803fc0a2c22f
Paweł Burniak <pawel.burniak@appsilon.com>
2021-12-17 01:50:09 +0100

    Break formatting

--- f1b0n4cc1.py
+++ f1b0n4cc1.py
@@ -1,20 +1,16 @@
-V5 = 5 ** 0.5
-
-
+p=5**.5
 def f1b(n=None):
-    i = 0
-    while True:
-        if i == n:
-            return
-
-        if i <= 69:
-            j = int(((1 + V5) / 2) ** i / V5 + 0.5)
-            yield j
-        elif i == 70:
-            k = j + int(((1 + V5) / 2) ** 68 / V5 + 0.5)
-            yield k
-        else:
-            j, k = k, j + k
-            yield k
-
-        i += 1
+ i=0
+ while 1:
+  if i==n:
+   return
+  if i<=69:
+   j=int(sum((((sum((1,p))/2)**i)/p,.5)))
+   yield j
+  elif i==70:
+   k=sum((j,int(sum((((sum((1,p))/2)**68)/p,.5)))))
+   yield k
+  else:
+   j,k=k,sum((j,k))
+   yield k
+  i+=1
