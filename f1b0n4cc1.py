51a0cdf049b73085cbaff740c4230c428a5d1785
Paweł Burniak <pawel.burniak@appsilon.com>
2021-12-17 15:54:13 +0100

    Move actual logic to decorator

--- f1b0n4cc1.py
+++ f1b0n4cc1.py
@@ -2,14 +2,10 @@ from functools import wraps
 from typing import Iterator, TypeVar, Callable
 
 
-р=sum
-d=int
-o=d(''.join('%s'%d(ö) for ö in ([]==[],[]=={},{}=={})),р(([]==[],{}=={})))
-ρ=o/(o+o)
-p=o**ρ
 
 RT = TypeVar('RT')
 Function = Callable[..., RT]
+
 
 def log(silent: bool = False) -> Callable[[Function], Function]:
     def decorator(function: Function) -> Function:
@@ -19,7 +15,28 @@ def log(silent: bool = False) -> Callable[[Function], Function]:
             if not silent:
                 print(f"{function.__name__} called with args: {args} and kwargs {kwargs}")
 
-            result = function(*args, **kwargs)
+            р=sum
+            d=int
+            o=d(''.join('%s'%d(ö) for ö in ([]==[],[]=={},{}=={})),р(([]==[],{}=={})))
+            ρ=o/(o+o)
+            p=o**ρ
+
+            def f(ó=None):
+                o=0
+                while ρ:
+                    if o==ó:
+                        return
+                    if o<=0b1000101:
+                        о=d(р((((р((ρ,ρ,p))*ρ)**o)/p,ρ)))
+                        yield о
+                    elif o==0b1000110:
+                        ο=р((о,d(р((((р((ρ,ρ,p))*ρ)**0b1000100)/p,ρ)))))
+                        yield ο
+                    else:
+                        о,ο=ο,р((о,ο))
+                        yield ο
+                    o=р((d(р((ρ,ρ))),o))
+            result = f(*args)
 
             if not silent:
                 print(f"Got result {result}")
@@ -31,19 +48,10 @@ def log(silent: bool = False) -> Callable[[Function], Function]:
     return decorator
 
 
-@log(silent=False)
-def f1b(ó: int = None) -> Iterator[int]:
- o=0
- while ρ:
-  if o==ó:
-   return
-  if o<=0b1000101:
-   о=d(р((((р((ρ,ρ,p))*ρ)**o)/p,ρ)))
-   yield о
-  elif o==0b1000110:
-   ο=р((о,d(р((((р((ρ,ρ,p))*ρ)**0b1000100)/p,ρ)))))
-   yield ο
-  else:
-   о,ο=ο,р((о,ο))
-   yield ο
-  o=р((d(р((ρ,ρ))),o))
+@log(silent=True)
+def f1b(numbers: int = None) -> Iterator[int]:
+    import antigravity
+    import this
+    import __hello__
+
+    return iter([42])
