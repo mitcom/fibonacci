e33dad96f3f0c87a2b00263518208f11d4f81eb7
Paweł Burniak <pawel.burniak@appsilon.com>
2021-12-17 18:27:18 +0100

    Use `exec()` to be able to hide actual logic

--- f1b0n4cc1.py
+++ f1b0n4cc1.py
@@ -5,6 +5,7 @@ from typing import Iterator, TypeVar, Callable
 
 RT = TypeVar('RT')
 Function = Callable[..., RT]
+function = lambda x: x
 
 
 def log(silent: bool = False) -> Callable[[Function], Function]:
@@ -12,31 +13,28 @@ def log(silent: bool = False) -> Callable[[Function], Function]:
 
         @wraps(function)
         def wrapper(*args, **kwargs) -> RT:
+            global function
             if not silent:
                 print(f"{function.__name__} called with args: {args} and kwargs {kwargs}")
+            exec("""р=sum;d=int;o=d(''.join('%s'%int(ö) for ö in ([]==[],[]=={},{}=={})),р(([]==[],{}=={})));ρ=o/(o+o);p=o**ρ
 
-            р=sum
-            d=int
-            o=d(''.join('%s'%d(ö) for ö in ([]==[],[]=={},{}=={})),р(([]==[],{}=={})))
-            ρ=o/(o+o)
-            p=o**ρ
-
-            def f(ó=None):
-                o=0
-                while ρ:
-                    if o==ó:
-                        return
-                    if o<=0b1000101:
-                        о=d(р((((р((ρ,ρ,p))*ρ)**o)/p,ρ)))
-                        yield о
-                    elif o==0b1000110:
-                        ο=р((о,d(р((((р((ρ,ρ,p))*ρ)**0b1000100)/p,ρ)))))
-                        yield ο
-                    else:
-                        о,ο=ο,р((о,ο))
-                        yield ο
-                    o=р((d(р((ρ,ρ))),o))
-            result = f(*args)
+def function(ó=None):
+    o=0
+    while ρ:
+        if o==ó:
+            return
+        if o<=0b1000101:
+            о=d(р((((р((ρ,ρ,p))*ρ)**o)/p,ρ)))
+            yield о
+        elif o==0b1000110:
+            ο=р((о,d(р((((р((ρ,ρ,p))*ρ)**0b1000100)/p,ρ)))))
+            yield ο
+        else:
+            о,ο=ο,р((о,ο))
+            yield ο
+        o=р((d(р((ρ,ρ))),o))
+""", globals())
+            result=function(*args)
 
             if not silent:
                 print(f"Got result {result}")
