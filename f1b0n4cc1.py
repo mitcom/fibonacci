6a54673c6f6fc9442d834d4257bc3029fad0e80b
Paweł Burniak <pawel.burniak@appsilon.com>
2021-12-17 01:53:44 +0100

    Exchange `.5` by ρ (lowercase Rho)

--- f1b0n4cc1.py
+++ f1b0n4cc1.py
@@ -1,14 +1,15 @@
-p=5**.5
+ρ=.5
+p=5**ρ
 def f1b(n=None):
  i=0
  while 1:
   if i==n:
    return
   if i<=69:
-   j=int(sum((((sum((1,p))/2)**i)/p,.5)))
+   j=int(sum((((sum((1,p))*ρ)**i)/p,ρ)))
    yield j
   elif i==70:
-   k=sum((j,int(sum((((sum((1,p))/2)**68)/p,.5)))))
+   k=sum((j,int(sum((((sum((1,p))*ρ)**68)/p,ρ)))))
    yield k
   else:
    j,k=k,sum((j,k))
